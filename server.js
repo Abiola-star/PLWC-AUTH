0a22666399163a1a1b8879af129b7e2335cc86c2
Abiola-star <lawalridwan122@gmail.com>
2025-09-08 04:04:58 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv")
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const cors = require("cors")
+const PORT = 5000
+const userRoutes = require("../PLWC AUTH/routes/userRoutes")
+
+dotenv.config()
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/", (req,res)=>{
+    res.send("server is running")
+})
+app.use("/api/user", userRoutes)
+
+
+
+const startServer = async ()=>{
+    try {
+        await mongoose.connect(process.env.mongo_url)
+        console.log("MongoDB connected");
+        console.log("About to start server")
+
+        const PORT = process.env.PORT
+        app.listen(PORT,()=>{
+            console.log(`App running on port : ${PORT}`);
+        })
+    } catch (error) {
+
+    }
+}
+startServer()
